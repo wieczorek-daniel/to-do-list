33b6850f6584cb46f85303534eaea52e2618a569
Daniel Wieczorek <wieczorek.daniel@outlook.com>
2021-09-23 21:46:00 +0200

    login-and-authentication-system

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('login', views.login, name='login'),
-    path('register', views.register, name='register'),
+    path('login/', views.loginUser, name='login'),
+    path('register/', views.registerUser, name='register'),
+    path('logout/', views.logoutUser, name='logout'), 
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,24 +1,57 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .forms import CreateUserForm
 
 
 def index(request):
     return render(request, "main/index.html")
 
-def login(request):
-    return render(request, "main/login.html")
 
-def register(request):
-    form = CreateUserForm()
+def loginUser(request):
+	if request.user.is_authenticated:
+		return redirect('dashboard')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
 
-    if request.method == 'POST':
-         form = CreateUserForm(request.POST)
-         if form.is_valid():
-             form.save()
-             username = form.cleaned_data.get('username')
-             messages.success(request, f'An account has been created for user {username}.')
-             return redirect('login')
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('dashboard')
+			else:
+				messages.info(request, 'The username or password is incorrect.')
+
+		context = {}
+		return render(request, 'main/login.html', context)
+
+
+def registerUser(request):
+    if request.user.is_authenticated:
+	    return redirect('dashboard')
+    else:
+        form = CreateUserForm()
+
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                messages.success(request, f'An account has been created for user {username}.')
+                return redirect('login')
 
     context = {'form': form}
     return render(request, "main/register.html", context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, "main/dashboard.html")
